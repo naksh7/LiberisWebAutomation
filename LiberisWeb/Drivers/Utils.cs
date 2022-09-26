19453a84f3c88aacd5eefe550ecef2cfc40cc184
Akshay Nagpure <83220169+naksh7@users.noreply.github.com>
2022-09-26 06:40:46 +0000

    Add project files.

---
+++ LiberisWeb/Drivers/Utils.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace LiberisWeb.Drivers
+{
+    public class Utils
+    {
+        public static IWebDriver driver;
+        static string url = "https://www.liberis.com/";
+
+        public static IWebDriver getDriver()
+        {
+            driver = new ChromeDriver(getChromeDriverPath());
+            driver.Url = url;
+            return driver;
+        }
+        
+        public static void closeDriver()
+        {
+            driver.Close();
+        }
+        private static string getChromeDriverPath()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            return projectDirectory + "\\Drivers";
+        }
+        public static List<string> StringSplitter(string typeOfPartners)
+        {
+            return typeOfPartners.Split(',').ToList();
+        }
+    }
+}
