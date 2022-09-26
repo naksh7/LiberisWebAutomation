b5bf73e85d5b73e4ca240102687f7ac8d029baf0
Akshay Nagpure <83220169+naksh7@users.noreply.github.com>
2022-09-26 06:43:33 +0000

    Add project files.

---
+++ LiberisWeb/Hooks/MyHook.cs
@@ -0,0 +1,22 @@
+﻿using LiberisWeb.Drivers;
+using TechTalk.SpecFlow;
+
+namespace LiberisWeb.Hooks
+{
+    [Binding]
+    public class MyHooks
+    {
+        private ScenarioContext _scenarioContext;        
+
+        public MyHooks(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [AfterScenario]
+        public void closeDriver()
+        {
+           Utils.closeDriver();
+        }
+    }
+}
